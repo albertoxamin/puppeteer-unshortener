5315e927c817e6bea963864c200cedd739c785a8
Alberto Xamin <alberto@xamin.it>
2019-08-04 19:10:36 +0200

    skip images and css

--- chromium.js
+++ chromium.js
@@ -8,7 +8,17 @@ async function unshortUrl(url) {
         headless: chrome.headless,
     });
 
-    const page = await browser.newPage();
+	const page = await browser.newPage();
+	await page.setRequestInterception(true);
+	const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' + 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
+	await page.setUserAgent(userAgent);
+	page.on('request', (req) => {
+		if (req.resourceType() == 'stylesheet' || req.resourceType() == 'font' || req.resourceType() == 'image') {
+			req.abort()
+		} else {
+			req.continue()
+		}
+	});
 	await page.goto(url, { waitUntil: 'networkidle0' });
     return page.url();
 }
